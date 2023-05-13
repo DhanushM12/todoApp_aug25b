de6072565764386dcdd8a53945ce6d9abd8221a7
DhanushM12 <dhanushmukhi@gmail.com>
2023-05-13 11:58:22 +0530

    add functionality

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import '../App.css'
+import TodoMain from './TodoMain';
+
+function App() {
+  let time = new Date().toLocaleTimeString();
+  const [currentTime, setCurrentTime] = useState(time);
+  //const [stateName, setterMethod] = useState(initialValue)
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time)
+  }
+  setInterval(updateTime, 1000)
+  return (
+    <div className='App'>
+      <h1>Todo App</h1>
+      <h2>{ currentTime }</h2>
+      <TodoMain />
+    </div>
+  )
+}
+
+export default App
